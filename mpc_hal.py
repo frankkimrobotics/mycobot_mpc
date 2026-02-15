1a3d8abaf47f68dfbe23026c7256e8835dbc8d32
frankthetank91 <victovgb@gmail.com>
2026-02-15 11:15:27 -0500

    change duration sec

--- mpc_hal.py
+++ mpc_hal.py
@@ -459,8 +459,8 @@ def main():
         for i in range(5):
             init = [-90,-90,0,-90,0,0]
             target = [a + (i+1)*3.0 for a in init]
-            run_mpc_loop(h, s, init, duration_sec=10.0)
-            run_mpc_loop(h, s, target, duration_sec=10.0)
+            run_mpc_loop(h, s, init, duration_sec=5.0)
+            run_mpc_loop(h, s, target, duration_sec=5.0)
         # run_mpc_loop(h, s, target, duration_sec=10.0)
         # run_mpc_loop(h, s, current, duration_sec=10.0)
     except KeyboardInterrupt:
