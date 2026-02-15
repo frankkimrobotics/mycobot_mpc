c9ec913ecc66971f72da4ee27cd1dfe75b78bf79
frankthetank91 <victovgb@gmail.com>
2026-02-15 11:13:19 -0500

    changes to mpc setup

--- mpc_hal.py
+++ mpc_hal.py
@@ -456,11 +456,11 @@ def main():
 
     # Run (Ctrl+C to stop)
     try:
-        for i in range(1):
+        for i in range(5):
             init = [-90,-90,0,-90,0,0]
-            target = [a + 5.0 for a in init]
+            target = [a + (i+1)*3.0 for a in init]
             run_mpc_loop(h, s, init, duration_sec=10.0)
-            # run_mpc_loop(h, s, target, duration_sec=10.0)
+            run_mpc_loop(h, s, target, duration_sec=10.0)
         # run_mpc_loop(h, s, target, duration_sec=10.0)
         # run_mpc_loop(h, s, current, duration_sec=10.0)
     except KeyboardInterrupt:
